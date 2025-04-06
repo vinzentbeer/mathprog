26b35c968c6349411784a2fb2f6f7fa75402fb71
Martin Gogeißl <martin.gogeissl@st.oth-regensburg.de>
2025-04-06 23:57:01 +0200

    ex3 stuff

--- mathprog-ex1/src/mathprog_ex1/ex1.3.py
+++ mathprog-ex1/src/mathprog_ex1/ex1.3.py
@@ -14,13 +14,36 @@ def build_model(model: gp.Model, n: int, k: int):
     # to save a reference in the model itself
     #
     # model.addConstrs(...)
+
+    w1 = model.addVars(n, n, vtype=GRB.BINARY, name="winning variable 1 ")
+    w2 = model.addVars(n, n, vtype=GRB.BINARY, name="winning variable 2 ")
+    d1 = model.addVars(n, n, vtype=GRB.BINARY, name="draw variable 1 ")
+    d2 = model.addVars(n, n, vtype=GRB.BINARY, name="draw variable 2 ")
+
+    r = model.addVars(n, vtype=GRB.BINARY, name="relegation variable")
+    
+    kappa = model.addVar(lb=0, vtype=GRB.CONTINUOUS, name="threshold")
+
+    model.addConstrs(w1[i,j] + w1[j,i] + d1[i,j] == 1 for i in range(n) for j in range(n) if i != j)
+    model.addConstrs(w2[i,j] + w2[j,i] + d2[i,j] == 1 for i in range(n) for j in range(n) if i != j)
+
+    model.addConstr(r[0] == 0)
+
+    model.addConstr(gp.quicksum(r) == k)
+
+    M = 1000
+    model.addConstrs(gp.quicksum(3*w1[i,j]+3*w2[i,j]+d1[i,j]+d1[i,j] for j in range(n) if i!=j) <= kappa + 1 + M*(1-r[i]) for i in range(n))
+    model.addConstrs(gp.quicksum(3*w1[i,j]+3*w2[i,j]+d1[i,j]+d1[i,j] for j in range(n) if i!=j) >= kappa - M*r[i] for i in range(n))
+
+    model.setObjective(gp.quicksum(3*w1[0,j]+3*w2[0,j]+d1[0,j]+d1[0,j] for j in range(n) if j!=0), GRB.MINIMIZE)
+
     pass
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--n", default=18)
-    parser.add_argument("--k", default=3)
+    parser.add_argument("--n", default=5)
+    parser.add_argument("--k", default=1)
     args = parser.parse_args()
 
     model = gp.Model("ex1.3")
