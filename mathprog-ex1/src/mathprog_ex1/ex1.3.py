522e0fcb957793ad13168049d7c3f1b26587a5aa
Martin Gogeißl <martin.gogeissl@st.oth-regensburg.de>
2025-04-07 20:50:49 +0200

    ex3 comments

--- mathprog-ex1/src/mathprog_ex1/ex1.3.py
+++ mathprog-ex1/src/mathprog_ex1/ex1.3.py
@@ -14,19 +14,24 @@ def build_model(model: gp.Model, n: int, k: int):
     # to save a reference in the model itself
     #
     # model.addConstrs(...)
-
+    
+    # Match result variables
     w1 = model.addVars(n, n, vtype=GRB.BINARY, name="winning variable 1 ")
     w2 = model.addVars(n, n, vtype=GRB.BINARY, name="winning variable 2 ")
     d1 = model.addVars(n, n, vtype=GRB.BINARY, name="draw variable 1 ")
     d2 = model.addVars(n, n, vtype=GRB.BINARY, name="draw variable 2 ")
 
-    P = model.addVars(n, vtype=GRB.INTEGER, lb=0, name='points ')
+    # Point variable
+    P = model.addVars(n, vtype=GRB.INTEGER, lb=0, name='points')
 
+    # Only one outcome per match
     model.addConstrs(w1[i,j] + w1[j,i] + d1[i,j] == 1 for i in range(n) for j in range(n) if i != j)
     model.addConstrs(w2[i,j] + w2[j,i] + d2[i,j] == 1 for i in range(n) for j in range(n) if i != j)
 
-    model.addConstrs(P[i] == gp.quicksum(3*w1[i,j]+3*w2[i,j]+d1[i,j]+d1[i,j] for j in range(n) if i!=j) for i in range(n))
+    # Calculate points
+    model.addConstrs(P[i] == gp.quicksum(3*w1[i,j]+3*w2[i,j]+d1[i,j]+d2[i,j] for j in range(n) if i!=j) for i in range(n))
 
+    # Order points inccreasingly
     model.addConstrs(P[i] <= P[i+1] for i in range(n-1))
 
     model.setObjective(P[k-1] + 1, GRB.MAXIMIZE)
@@ -49,7 +54,6 @@ if __name__ == "__main__":
     if model.SolCount > 0:
         print(f"obj. value = {model.ObjVal}")
         for v in model.getVars():
-            #if v.VarName == 'points':
-                print(f"{v.VarName} = {v.X}")
+            print(f"{v.VarName} = {v.X}")
 
     model.close()
